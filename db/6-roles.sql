96f2a72dcbb634549aba43f7ddad281c482f3850
Yael <243678@ids.upchiapas.edu.mx>
2026-02-13 09:35:03 -0600

    roles

--- db/6-roles.sql
+++ db/6-roles.sql
@@ -9,6 +9,9 @@ NOCREATEROLE
 INHERIT;
 
 GRANT USAGE ON SCHEMA public TO moderador;
-GRANT SELECT ON ordenes TO moderador;
-GRANT SELECT ON usuarios TO moderador;
+GRANT SELECT ON vw_stock_vendido TO moderador;
+GRANT SELECT ON vw_ventas_usuarios TO moderador;
+GRANT SELECT ON vw_ventas_categorias TO moderador;
+GRANT SELECT ON vw_ranking_productos TO moderador;
+GRANT SELECT ON vw_usuarios_pagudos TO moderador;
 -- dar permiso para los reportes (cuando los haga)
