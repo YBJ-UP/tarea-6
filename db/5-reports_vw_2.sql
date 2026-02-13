49c90ac4dc462d5679a38c5f0e9a72aa1d5235cf
Yael <243678@ids.upchiapas.edu.mx>
2026-02-12 22:32:29 -0600

    masomenos (no jala dx)

---
+++ db/5-reports_vw_2.sql
@@ -0,0 +1,19 @@
+--REPORTE 1
+--¿Qué devuelve? Los usuarios que han realizado compras, sin importar el estado de estas, pero si estan canceladas o pendientes que se muestre algo
+--Grain: Cada fila es un usuario
+--Métricas: Cantidad de compras realizadas y el dinero gastado
+--¿Por qué GROUP BY/HAVING?
+--  ahi voy
+CREATE OR REPLACE VIEW vw_ventas_usuarios AS
+    SELECT
+        u.nombre,
+        u.email,
+        COALESCE( SUM( o.total ), 0 ) AS total_gastado,
+        COALESCE( ROUND( AVG( o.total ), 2 ), 0 ) AS promedio_gastado
+        COUNT( DISTINCT o.id ) AS compras_realizadas,
+        MAX( o.created_at ) AS ultima_compra
+    FROM usuarios u
+    JOIN ordenes o ON u.id = o.usuario_id
+    GROUP BY u.nombre, u.email
+    HAVING COUNT( DISTINCT o.id ) > 0
+    ORDER BY COALESCE( SUM( o.total ), 0 );
