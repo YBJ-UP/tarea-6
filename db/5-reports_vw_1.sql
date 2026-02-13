4bc07d46b74352afa8176116fdf26de022df95a4
Yael <243678@ids.upchiapas.edu.mx>
2026-02-12 21:56:47 -0600

    primer reporte completado y con 1 verificación

--- db/5-reports_vw_1.sql
+++ db/5-reports_vw_1.sql
@@ -8,10 +8,18 @@ CREATE OR REPLACE VIEW vw_stock_vendido AS
         p.codigo,
         p.nombre,
         p.stock AS stock_disponible,
+        p.precio,
         COALESCE( SUM( od.cantidad ), 0 ) AS unidades_vendidas,
-        p.precio * COALESCE( od.cantidad, 0 ) AS dinero_generado,
+        p.precio * COALESCE( SUM( od.cantidad ), 0 ) AS dinero_generado,
         CASE
             WHEN p.stock = 0 THEN 'AGOTADO'
-            WHEN p.stock < 20 THEN 'BAJO'
+            WHEN p.stock < 50 THEN 'BAJO'
             WHEN COALESCE( SUM( od.cantidad ), 0 ) = 0 THEN 'SIN VENTAS'
             ELSE 'NORMAL'
+        END AS estado
+    FROM productos p
+    JOIN orden_detalles od ON p.id = od.producto_id
+    GROUP BY p.codigo, p.nombre, p.stock, p.precio
+    ORDER BY p.stock DESC;
+
+SELECT * FROM vw_stock_vendido;
