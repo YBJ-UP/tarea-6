bd64f6ac378248228488a92ebd892adaf7c47f93
Yael <243678@ids.upchiapas.edu.mx>
2026-02-13 01:10:04 -0600

    ahora si

--- db/5-reports_vw_5.sql
+++ db/5-reports_vw_5.sql
@@ -4,6 +4,7 @@
 --Métricas: a
 --¿Por qué GROUP BY/HAVING?
 --  Se utiliza GROUP BY para que agrupe filas con los mismos valores segun lo que se especifica
+--  Se usa HAVING para filtrar por los usuarios que hayan gastado más del promedio
 CREATE OR REPLACE VIEW vw_usuarios_pagudos AS
     WITH promedio_tienda AS (
         SELECT
@@ -11,4 +12,14 @@ CREATE OR REPLACE VIEW vw_usuarios_pagudos AS
         FROM ordenes o
     )
     SELECT
-        u.nombre
+        u.nombre,
+        u.email,
+        COUNT( DISTINCT o.id ) as ordenes_totales,
+        SUM( o.total ) as total_gastado
+    FROM usuarios u
+    JOIN ordenes o ON u.id = o.usuario_id
+    GROUP BY u.nombre, u.email
+    ORDER BY SUM( o.total )
+    HAVING SUM( o.total ) > (SELECT total_gastado FROM promedio_tienda);
+
+SELECT * FROM vw_usuarios_pagudos;
