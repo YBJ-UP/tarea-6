5923e98bbd3be1415fef100fea7e6150053d9ff1
Yael <243678@ids.upchiapas.edu.mx>
2026-02-13 00:20:56 -0600

    creo que ese reporte ya

--- db/5-reports_vw_4.sql
+++ db/5-reports_vw_4.sql
@@ -1,16 +1,16 @@
 --REPORTE 4
 --¿Qué devuelve? un ranking de productos segun lo que han vendido
 --Grain: Cada fila es un producto
---Métricas: a
+--Métricas: el total generado y el ranking
 --¿Por qué GROUP BY/HAVING?
---  a
+--  Se utiliza GROUP BY para que agrupe filas con los mismos valores segun lo que se especifica
 CREATE OR REPLACE VIEW vw_ranking_productos AS
     SELECT
         p.nombre as producto,
         c.nombre as categoria,
         SUM( od.subtotal ) AS total_generado,
         RANK() OVER ( ORDER BY SUM( od.subtotal ) DESC ) AS ranking,
-        p.stock AS stock_disponible,
+        p.stock AS stock_disponible
     FROM productos p
     JOIN categorias c ON p.categoria_id = c.id
     JOIN orden_detalles od on p.id = od.producto_id
